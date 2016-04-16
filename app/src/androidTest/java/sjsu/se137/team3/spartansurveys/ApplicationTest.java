996c7fecb316079a574cd1805df6475e2d5eb3ef
Krystle Weinrich <krystleweinrich@yahoo.com>
2016-04-15 19:12:30 -0700

    commit the initial things

---
+++ app/src/androidTest/java/sjsu/se137/team3/spartansurveys/ApplicationTest.java
@@ -0,0 +1,13 @@
+package sjsu.se137.team3.spartansurveys;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
