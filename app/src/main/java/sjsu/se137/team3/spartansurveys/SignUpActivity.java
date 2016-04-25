acdb54b5e53ebf6bcfbfef82b11a08b2602e36ad
Krystle Weinrich <krystleweinrich@yahoo.com>
2016-04-25 11:47:33 -0700

    added views for Welcome, Sign up. they are empty atm

---
+++ app/src/main/java/sjsu/se137/team3/spartansurveys/SignUpActivity.java
@@ -0,0 +1,8 @@
+package sjsu.se137.team3.spartansurveys;
+
+/**
+ * Created by Krystle on 4/25/2016.
+ *
+ */
+public class SignUpActivity {
+}
---
+++ app/src/main/java/sjsu/se137/team3/spartansurveys/WelcomeActivity.java
@@ -0,0 +1,8 @@
+package sjsu.se137.team3.spartansurveys;
+
+/**
+ * Created by Krystle on 4/25/2016.
+ * This is the page where you click sign up or sign in.
+ */
+public class WelcomeActivity {
+}
