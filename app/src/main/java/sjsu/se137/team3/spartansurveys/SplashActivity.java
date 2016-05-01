392b240f09799600bca32544bed6eac0c4a56d52
Krystle Weinrich <krystleweinrich@yahoo.com>
2016-04-30 17:17:06 -0700

    buttons are pretty, launch screen is pretty. internet permissions are enabled

---
+++ app/src/main/java/sjsu/se137/team3/spartansurveys/SplashActivity.java
@@ -0,0 +1,18 @@
+package sjsu.se137.team3.spartansurveys;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+//This is just the splash screen will forward you to the welcome activity
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(SplashActivity.this, WelcomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
